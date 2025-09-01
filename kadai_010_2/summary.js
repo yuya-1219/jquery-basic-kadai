121c455a6e440356ca07492d1039a0f5710bcc86
yuya-1219 <o.yuuya1219@gmail.com>
2025-09-01 11:01:53 +0900

    Add files via upload

---
+++ kadai_010_2/summary.js
@@ -0,0 +1,15 @@
+$('#change-color').on('click', function() {
+  $('#target').css('color', 'red');
+});
+
+$('#change-text').on('click', function() {
+  $('#target').text("Hello!");
+});
+
+$('#fade-out').on('click', function() {
+  $('#target').fadeOut();
+});
+
+$('#fade-in').on('click', function() {
+  $('#target').fadeIn();
+});
